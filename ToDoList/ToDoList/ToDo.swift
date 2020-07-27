5e22b7af7e23a3c8c3d0732ca2fc2ae2903170e0
sienna-yazdi <68655082+sienna-yazdi@users.noreply.github.com>
2020-07-27 13:40:44 -0500

    Add files via upload

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Apple on 7/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass{
+   
+    var description = " "
+    var important = false
+
+}
